0a1511fe6b96bd58de0cab5140f250c6b5d84e8f
Sam Norton <samnorton@Sams-Mac-mini.local>
2021-09-13 16:25:34 +0800

    Added assets and constants settings

---
+++ constants/icons.js
@@ -0,0 +1,9 @@
+export const book = require('../assets/icons/book.png');
+export const back = require('../assets/icons/back.png');
+export const search = require('../assets/icons/search.png');
+
+export default {
+  book,
+  back,
+  search,
+};
