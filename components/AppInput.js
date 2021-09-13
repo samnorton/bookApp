1d6b185cff1f18220c35312a1abfe86aae97ba9b
Sam Norton <samnorton@Sams-Mac-mini.local>
2021-09-13 21:04:52 +0800

    Added components boilerplate

---
+++ components/AppInput.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+class AppInput extends Component {
+  render() {
+    return (
+      <View>
+        <Text>App Input</Text>
+      </View>
+    );
+  }
+}
+
+export default AppInput;
---
+++ components/AppScreen.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+class AppScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>App Screen</Text>
+      </View>
+    );
+  }
+}
+
+export default AppScreen;
